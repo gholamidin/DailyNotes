dc5229ccb978a56700d460e5d92a6c33e90bb392
gholamidin <gholamidin@outlook.com>
2018-09-13 23:33:52 +0800

    leetcode

--- Set,Map,Hashmap/350.IntersectionOfTwoArraysII.cpp
+++ Set,Map,Hashmap/350.IntersectionOfTwoArraysII.cpp
@@ -1,6 +1,31 @@
 
 #include<iostream>
 #include<vector>
-#include<set>
-
+#include<map>
 using namespace std;
+
+std::vector<int>intersect(std::vector<int> &nums1,std::vector<int> &nums2){
+	map<int,int>record;
+	std::vector<int> res;
+	for(int i = 0 ; i < nums1.size() ; i++)
+		record[nums1[i]]++; //频数加1
+
+	for (int i = 0; i < nums2.size(); ++i)
+	{
+		if(record[nums2[i]] > 0 ){
+			res.push_back(nums2[i]);
+			record[nums2[i]]--;
+		}
+	}
+	return res;
+}
+
+int main(int argc, char const *argv[])
+{
+	std::vector<int> nums1 = {1,2,3,4,5,6,7,7};
+	std::vector<int> nums2 = {2,3,4,7};
+	std::vector<int> res =intersect(nums1,nums2);
+	for(std::vector<int>:: iterator i = res.begin() ; i != res.end() ; i ++)
+		cout << *i <<",";
+	return 0;
+}
